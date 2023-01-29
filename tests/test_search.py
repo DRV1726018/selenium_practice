14db5da8cdd0595df905573259dbef62f5913733
davidrv <davidramirezvillegas@gmail.com>
2023-01-29 11:43:03 -0500

    First automation test

--- tests/test_search.py
+++ tests/test_search.py
@@ -10,6 +10,7 @@ def test_basic_duckduckgo_search(browser):
     search_page = DuckDuckGoSearchPage(browser)
     result_page = DuckDuckGoResultPage(browser)
     PHRASE = "panda"
+
     # Given the DuckDuckGo home page is displayed
     search_page.load()
 
@@ -26,5 +27,3 @@ def test_basic_duckduckgo_search(browser):
     titles = result_page.result_link_titles()
     matches = [t for t in titles if PHRASE.lower() in t.lower()]
     assert len(matches) > 0
-
-    raise Exception("Incomplete Test")
